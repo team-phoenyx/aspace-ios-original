6663cf5cd674bab4de6cdb5b190270bd10ec3f88
Terrance Li <terranceli.terbear@gmail.com>
2017-07-27 11:19:27 -0700

    WIP tutorials location suggestions

---
+++ aspace/aspace/GeocoderResponse.swift
@@ -0,0 +1,9 @@
+//
+//  GeocoderResponse.swift
+//  aspace
+//
+//  Created by Terrance Li on 7/26/17.
+//  Copyright © 2017 aspace. All rights reserved.
+//
+
+import Foundation
--- aspace/aspace/TutorialLocationsViewController.swift
+++ aspace/aspace/TutorialLocationsViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SearchTextField
 
 class TutorialLocationsViewController: UIViewController {
     
@@ -15,26 +16,13 @@ class TutorialLocationsViewController: UIViewController {
     var homeAddress: String!
     var workAddress: String!
 
+    @IBOutlet weak var homeAddressTextField: SearchTextField!
+    @IBOutlet weak var workAddressTextField: SearchTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
